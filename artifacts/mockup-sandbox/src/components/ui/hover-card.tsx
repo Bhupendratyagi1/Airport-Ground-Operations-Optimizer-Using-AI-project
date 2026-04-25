f39d6ad6377b5cea0b0a8129ede750e45589b481
Bhupendra Tyagi <tyagibhupendra963@gmail.com>
2026-04-25 12:26:50 +0530

    Remove extra whitespace in hover-card.tsx

--- artifacts/mockup-sandbox/src/components/ui/hover-card.tsx
+++ artifacts/mockup-sandbox/src/components/ui/hover-card.tsx
@@ -25,3 +25,10 @@ const HoverCardContent = React.forwardRef<
 HoverCardContent.displayName = HoverCardPrimitive.Content.displayName
 
 export { HoverCard, HoverCardTrigger, HoverCardContent }
+
+
+
+
+
+
+
